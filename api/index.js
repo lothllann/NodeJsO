9b91ee4d7fd27d86d87c548de204a33144986139
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-09-06 14:40:18 -0300

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
+
+app.get('/teste', (req, res) => res
+    .status(200)
+    .send({ mensagem: 'boas vindas' })
+)
+
+
+
+module.exports = app;
